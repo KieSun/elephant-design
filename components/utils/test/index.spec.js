bfebf56069f023eb7e440b348c2b0d1829fe4b2c
Kiesun <zx597813039@gmail.com>
2019-06-12 10:53:10 +0800

    test: bem test

--- components/utils/test/index.spec.js
+++ components/utils/test/index.spec.js
@@ -4,6 +4,9 @@ import { bem } from '../index'
 test('className', () => {
   const b = bem('loading')
   expect(classNames(b('text'))).toEqual('eleph-loading__text')
+  expect(classNames(b(['disable']))).toEqual(
+    'eleph-loading eleph-loading--disable'
+  )
   expect(classNames(b('text', { disable: true }))).toEqual(
     'eleph-loading__text eleph-loading__text--disable'
   )
