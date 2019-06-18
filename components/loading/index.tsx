251b04560fb3f447e5d73085838757db6f3c3d87
Kiesun <zx597813039@gmail.com>
2019-06-18 19:04:01 +0800

    feat: add loading component feature

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,19 +1,48 @@
 import * as React from 'react'
+import classNames from 'classnames'
+import { bem } from '../utils'
+import './index.less'
 
 interface LoadingProps {
-  size?: 'small' | 'default' | 'large'
+  size?: number
   vertical?: Boolean
-  color?: string
+  indicatorColor?: string
   text?: string
   toast?: Boolean
   indicator?: React.ReactElement<any>
 }
 
-const defaultIndicator = null
+const wrapper = bem('loading')
+
+const defaultIndicator = <span className={classNames(wrapper('indicator'))} />
+
+function renderIndicator(props: LoadingProps) {
+  const { indicator, indicatorColor = '#1989FA', size = 26 } = props
+  if (React.isValidElement(indicator)) {
+    return React.cloneElement(indicator)
+  }
+  return React.cloneElement(defaultIndicator, {
+    style: {
+      borderColor: indicatorColor,
+      borderBottomColor: 'transparent',
+      width: `${size}px`,
+      height: `${size}px`
+    }
+  })
+}
 
 const Loading = (props: LoadingProps) => {
-  console.log(props, defaultIndicator)
-  return <div>1</div>
+  const { text, vertical } = props
+  return (
+    <div className={classNames(wrapper({ vertical }))}>
+      {renderIndicator(props)}
+      {text ? (
+        <span className={classNames(wrapper('text', { vertical }))}>
+          {text}
+        </span>
+      ) : null}
+    </div>
+  )
 }
 
 export default Loading
