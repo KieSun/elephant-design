903bd653ebc04ad7dcb350e80e75994a144415e2
KieSun <zx597813039@gmail.com>
2019-08-03 23:03:17 +0800

    test: add formatSize function test

--- components/utils/test/index.spec.js
+++ components/utils/test/index.spec.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { bem } from '../index'
+import { formatSize } from '../style'
 
 test('className', () => {
   const b = bem('loading')
@@ -10,4 +11,9 @@ test('className', () => {
   expect(classNames(b('text', { disable: true }))).toEqual(
     'eleph-loading__text eleph-loading__text--disable'
   )
+})
+
+test('formatSize', () => {
+  expect(formatSize(20)).toEqual('20px')
+  expect(formatSize('20px')).toEqual('20px')
 })
