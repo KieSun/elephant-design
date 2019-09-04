fc57c50fdced56091d9107037ae4f7a37de4b72f
Kiesun <zx597813039@gmail.com>
2019-09-04 16:17:46 +0800

    feat: slider drag logic

--- components/slider/index.tsx
+++ components/slider/index.tsx
@@ -15,30 +15,48 @@ interface TouchData {
   startY: number
   offsetX?: number
   offsetY?: number
+  startValue: number
 }
 
 const wrapperClass = bem('slider')
 
 type touchEvent = React.TouchEvent<HTMLDivElement>
 
+function limitNumber(number: number, min: number, max: number) {
+  return Math.min(Math.max(number, min), max)
+}
+
 const Slider = ({ value = 0, min = 0, max = 100, vertical }: SliderProps) => {
   const [touchState, setTouchState] = React.useState({} as TouchData)
+  const bar = React.useRef<HTMLDivElement>(null)
+  const [diffState, setDiffState] = React.useState(0)
 
   function handleTouchStart(e: touchEvent) {
     const touch = e.touches[0]
     setTouchState({
       startX: touch.clientX,
-      startY: touch.clientY
+      startY: touch.clientY,
+      startValue: diffState
     })
   }
 
   function handleTouchMove(e: touchEvent) {
-    const touch = e.touches[0]
-    setTouchState({
-      ...touchState,
-      offsetX: touch.clientX - touchState.startX,
-      offsetY: touch.clientY - touchState.startY
-    })
+    if (bar && bar.current) {
+      const rect = bar.current.getBoundingClientRect()
+      const { width } = rect
+      const { startX, startY, startValue } = touchState
+      const touch = e.touches[0]
+      const offsetX = touch.clientX - startX
+      const offsetY = touch.clientY - startY
+      let diff = offsetX / width
+      diff = startValue + diff
+      setDiffState(limitNumber(diff, 0, 1))
+      setTouchState({
+        ...touchState,
+        offsetX,
+        offsetY
+      })
+    }
   }
 
   function handleTouchEnd(e: touchEvent) {
@@ -46,8 +64,11 @@ const Slider = ({ value = 0, min = 0, max = 100, vertical }: SliderProps) => {
   }
 
   return (
-    <div className={classNames(wrapperClass())}>
-      <div className={classNames(wrapperClass('bar'))}>
+    <div className={classNames(wrapperClass())} ref={bar}>
+      <div
+        className={classNames(wrapperClass('bar'))}
+        style={{ width: `${diffState * 100}%` }}
+      >
         <div
           className={classNames(wrapperClass('button-wrapper'))}
           aria-valuemin={min}
