a90f6c0663b6d07de5563a7d9f112dd69276f3ed
Kiesun <zx597813039@gmail.com>
2019-09-05 18:00:18 +0800

    feat: improve slider example

--- example/src/pages/slider.tsx
+++ example/src/pages/slider.tsx
@@ -3,6 +3,14 @@ import { Slider } from '../../../dist/index.es'
 
 export default () => (
   <div>
-    <Slider />
+    <Slider
+      value={50}
+      min={40}
+      max={80}
+      step={4}
+      onChange={(value: number) => {
+        console.log(value)
+      }}
+    />
   </div>
 )
